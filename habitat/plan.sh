cb1aeed42ad5f5c58fd8c7638fd377ca434180ae
jtimberman <joshua@chef.io>
2018-03-15 13:31:39 -0600

    run as the root group, too

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -7,6 +7,7 @@ pkg_license=("Apache-2.0")
 pkg_source="http://some_source_url/releases/${pkg_name}-${pkg_version}.tar.gz"
 pkg_deps=(core/vault)
 pkg_svc_user="root"
+pkg_svc_group="root"
 
 do_download() {
   return 0
